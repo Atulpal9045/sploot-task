d2a02616ca3b27a80be486f1af3d982bf510fa93
Atul Pal <atulpal@Atuls-MacBook-Air.local>
2025-01-16 01:59:45 +0530

    fixed creator issue

--- pages/api/blogs/[id].js
+++ pages/api/blogs/[id].js
@@ -3,7 +3,7 @@ import User from "../models/User";
 import connectDB from "../utils/db";
 
 export default async function handler(req, res) {
-    console.log('here-----1')
+  console.log('here-----1');
   await connectDB();
   const { method } = req;
   const { id } = req.query;  // Use req.query to get the dynamic parameter
@@ -12,9 +12,19 @@ export default async function handler(req, res) {
   if (method === 'GET') {
     try {
       const blog = await Blog.findById(id)
-      const user = await User.findById(blog.createdBy)
-      blog["email"] = user?.email
-      blog["name"] = user?.name
+        .populate('createdBy', 'name email') // Populate directly here
+        .exec();
+
+      // Check if the blog and its creator exist
+      if (!blog) {
+        return res.status(404).json({ statusCode: 404, message: 'Blog not found' });
+      }
+
+      // If createdBy is missing, you can set the fields to null
+      if (!blog.createdBy) {
+        blog.createdBy = { name: null, email: null };
+      }
+
       res.status(200).json({ statusCode: 200, data: blog });
     } catch (error) {
       res.status(500).json({ statusCode: 500, message: error?.message || 'Internal Server Error!' });
